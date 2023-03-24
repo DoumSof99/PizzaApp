e8e2d78064ad22cfa2849976da1414d147566760
DoumSof99 <sofiadmn99@gmail.com>
2023-03-24 21:39:31 +0200

    initial

---
+++ PizzaAppEF/PizzaAppEF/Data/PizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPage.Models;
+
+namespace RazorPage.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=PizzaDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
