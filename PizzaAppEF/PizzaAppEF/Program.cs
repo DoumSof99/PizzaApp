ec850d1574b7041817ec1d8902cd0a2b55b9bd79
DoumSof99 <sofiadmn99@gmail.com>
2023-03-26 18:16:19 +0300

    Finished

--- PizzaAppEF/PizzaAppEF/Program.cs
+++ PizzaAppEF/PizzaAppEF/Program.cs
@@ -1,46 +1,70 @@
 ﻿using RazorPage.Data;
 using RazorPage.Models;
 
-
-// Run this part of code once to create rows in the product table
-
 using PizzaContext context = new PizzaContext();
 
-//Product vegetarianPizza = new Product()
-//{
-//    Name = "Veggie Pizza",
-//    Price = 9.99m
-//};
-//context.Products.Add(vegetarianPizza);
+#region This part of code create rows in the Product table
+Product vegetarianPizza = new Product()
+{
+    Name = "Veggie Pizza",
+    Price = 9.99m
+};
+context.Products.Add(vegetarianPizza);
 
-//Product deluxePizza = new Product()
-//{
-//    Name = "Deluce Pizza",
-//    Price = 13.20m
-//};
-//context.Products.Add(deluxePizza);
+Product deluxePizza = new Product()
+{
+    Name = "Deluce Pizza",
+    Price = 13.20m
+};
+context.Products.Add(deluxePizza);
 
 //context.SaveChanges();
-
+#endregion
 //--------------------------------------------
-//This part is for updating an entity in the DB
+#region This part of code create rows in the Customer table
+Customer customer1 = new Customer()
+{
+    FirstName = "Sofia", 
+    LastName = "Doumani",
+    Address = "Somewhere 9",
+    Phone = "6912345678",
+    Email = "Sof@gmail.com"
+};
+context.Customers.Add(customer1);
 
-//var vegPizza = context.Products.Where(p => p.Name == "Veggie Pizza").FirstOrDefault();
-
-//if (vegPizza is Product) { vegPizza.Price = 10.99m; }
+Customer customer2 = new Customer()
+{
+    FirstName = "Patrick",
+    LastName = "Hunt",
+    Address = "Microsoft 10",
+    Phone = "6987654321",
+    Email = "Patrick@gmail.com"
+};
+context.Customers.Add(customer2);
 
 //context.SaveChanges();
 
+#endregion
 //--------------------------------------------
-//This part is for deleting an entity in the DB
+#region This part is for updating an entity in the DB
 
-//var vegPizza = context.Products.Where(p => p.Name == "Veggie Pizza").FirstOrDefault();
+var vegPizza1 = context.Products.Where(p => p.Name == "Veggie Pizza").FirstOrDefault();
 
-//if (vegPizza is Product) { context.Remove(vegPizza); }
+if (vegPizza1 is Product) { vegPizza1.Price = 10.99m; }
 
 //context.SaveChanges();
+#endregion
+//--------------------------------------------
+#region This part is for removing an entity in the DB
 
+var vegPizza2 = context.Products.Where(p => p.Name == "Veggie Pizza").FirstOrDefault();
 
+if (vegPizza2 is Product) { context.Remove(vegPizza2); }
+
+//context.SaveChanges();
+#endregion
+
+#region This part id for displaying the above data with some statements
 var products = from product in context.Products
                where product.Price > 10.00m
                orderby product.Name
@@ -48,8 +72,9 @@ var products = from product in context.Products
 
 foreach (Product p in products)
 {
-    Console.WriteLine($"Id:{p.Id}");
-    Console.WriteLine($"Name:{p.Name}");
-    Console.WriteLine($"Price:{p.Price}");
-    Console.WriteLine(new string('-', 20));
+    //Console.WriteLine($"Id:{p.Id}");
+    //Console.WriteLine($"Name:{p.Name}");
+    //Console.WriteLine($"Price:{p.Price}");
+    //Console.WriteLine(new string('-', 20));
 }
+#endregion
